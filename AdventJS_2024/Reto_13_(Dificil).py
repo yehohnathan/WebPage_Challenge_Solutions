09848c6bbd050e613998cabc3296d861badc6629
yehohnathan <yehohnathan2002@gmail.com>
2024-12-12 23:19:26 -0600

    Logrado a la primera :D
    Signed-off-by: yehohnathan <yehohnathan2002@gmail.com>

--- AdventJS_2024/Reto_13_(Dificil).py
+++ AdventJS_2024/Reto_13_(Dificil).py
@@ -33,8 +33,33 @@ posición donde se detuvo
 
 # ++++++++++++++++++++++++++++++ # FUNCIONES # ++++++++++++++++++++++++++++++ #
 def is_robot_back(moves: list[str]) -> bool | list[int]:
-    # Code here
-    return True
+    # Convierte move en una lista para su facil manipulación (reemplazar)
+    moves = list(moves)
+    # Variable que contiene la posición del rebot
+    posicion = [0, 0]
+    # Diccionario para contar los movimientos
+    contador_moves = {'R': 0, 'L': 0, 'U': 0, 'D': 0}
+    # Diccionario movimientos opuestos
+    opuesto_moves = {'R': 'L', 'L': 'R', 'U': 'D', 'D': 'U'}
+    # Ciclo para los movimientos del robot
+    mod = 1    # Modificador del movimiento
+    for i, char in enumerate(moves):
+        if char == '*':
+            mod = 2
+        elif char == '?':
+            if contador_moves[moves[i + 1]] != 0:
+                mod = 0
+        elif char == '!':
+            moves[i + 1] = opuesto_moves[moves[i + 1]]
+        else:
+            contador_moves[char] += 1 * mod
+            mod = 1
+
+    # Se suma los movimientos
+    posicion[0] = contador_moves['R'] - contador_moves['L']
+    posicion[1] = contador_moves['U'] - contador_moves['D']
+
+    return True if posicion == [0, 0] else posicion
 
 
 # +++++++++++++++++++++++++++++++++ # MAIN # ++++++++++++++++++++++++++++++++ #
